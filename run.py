11851e8cd8257231a948cedcc05efcc80d20f9a1
Florian Sassmann <florian.sassmann@hotmail.com>
2024-08-28 13:21:22 +0000

    Add gsheet and creds and variables for words_sheet

---
+++ run.py
@@ -0,0 +1,17 @@
+# Imports
+import gspread
+from google.oauth2.service_account import Credentials
+import random
+
+# Variables for google sheet
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("words_sheet")
+WORD_SHEET = SHEET.worksheet("words")
