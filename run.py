d9747450dd6b6384799cf05e162ca5605e73e3b8
Florian Sassmann <florian.sassmann@hotmail.com>
2024-08-28 14:07:45 +0000

    Add main menu function

--- run.py
+++ run.py
@@ -15,3 +15,45 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("words_sheet")
 WORD_SHEET = SHEET.worksheet("words")
+
+def main_menu():
+    """
+    Main Menu function
+    Takes user input and runs user choice
+    """
+    print("[1] Play Hangman")
+    print("[2] Game Instructions")
+    print("[3] maybe i add something later")
+    print("[0] Exit the program")
+
+main_menu()
+choice = int(input("Enter your option: "))
+
+while choice != 0:
+    if choice == 1:
+        # Opens Hangman Game
+        print("You choose 1, Game will start shortly....")
+    elif choice == 2:
+        # Opens Game Instructions
+        print("You choose 2, Game Instructions will show in a few seconds....")
+    elif choice == 3:
+        # maybe there is a 3th Option :D
+        print("Nothing to see in choice 3, maybe later..")
+    else: 
+        print("choice not available")
+    
+    print()
+    main_menu()
+    choice = int(input("Enter your option: "))
+
+print("Thanks for playing the game")
+
+
+"""
+valid_choices = ["1", "2", "3"]
+
+while choice not in valid_choices:
+    main_menu()
+
+    tried with validator - doesn't work at the moment, will comeback to it later
+"""
